e81267bf1059b9c729d73744e717e1d10c9d4a8c
Andy Linton <ajlinton@hotmail.com>
2024-07-05 18:00:20 -0700

    Initial

---
+++ Scripts/Pictures.js
@@ -0,0 +1,41 @@
+﻿var filenames;
+var start = 0;
+var picturesPerPage = 5;
+
+function next() {
+    displayPictures();
+}
+
+function displayPictures() {
+    var tableText = "<div id='tableContent'>";
+    for (var i = 0; i < picturesPerPage; i++) {
+        tableText = tableText + "<div><a href='" + filenames[start] + "' target='_blank'>";
+        tableText = tableText + "<img class='img1' alt='Image' id='Image" + i + "' src='" + filenames[start] + "'  />";
+        tableText = tableText + "</a></div>";
+        start++;
+        if (start == filenames.length) {
+            start = 0;
+            break;
+        }
+    }
+    tableText = tableText + "</div>";
+    var table = document.getElementById("tableContent");
+    table.innerHTML = tableText;
+}
+
+function getPictureFilenames() {
+    $.ajax({
+        type: 'post',
+        url: getPicturesUrl,
+        dataType: 'json',
+        success: function (response) {
+            filenames = response;
+            displayPictures();
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            debugger;
+        }
+    });
+}
+
+
